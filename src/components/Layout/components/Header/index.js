dc354decd25de7127eee1a04e2b4b43fff10c60b
hoangquan-061199 <hoangquan061199@gmail.com>
2022-09-07 09:44:08 +0700

    add className, CSS Layout, change favicon

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <div className={cx('wrapper')}>
+      <header className={cx('inner')}>
+        <h2>Header</h2>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
