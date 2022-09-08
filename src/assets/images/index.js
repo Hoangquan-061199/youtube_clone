e93d90bfc253575c3feb3b57a7395dd8d78c72fa
hoangquan-061199 <hoangquan061199@gmail.com>
2022-09-08 20:51:14 +0700

    Header UI #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+const images = {
+  logo: require('~/assets/images/logo.svg').default,
+  navBtn: require('~/assets/images/navBtn.svg').default,
+  search: require('~/assets/images/search.svg').default,
+  delete: require('~/assets/images/delete.svg').default,
+  voice: require('~/assets/images/voice.svg').default,
+  setting: require('~/assets/images/setting.svg').default,
+  user: require('~/assets/images/user.svg').default,
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +8,45 @@ function Header() {
   return (
     <div className={cx('wrapper')}>
       <header className={cx('inner')}>
-        <h2>Header</h2>
+        <div className={cx('logo')}>
+          <div className={cx('nav-btn')}>
+            <img src={images.navBtn} alt="BTN" />
+          </div>
+          <div className={cx('logo-youtube')}>
+            <img src={images.logo} alt="YouTube" />
+          </div>
+          <span className={cx('country-code')}>VN</span>
+        </div>
+        <div className={cx('search')}>
+          <div className={cx('search-form')}>
+            <div className={cx('search-box')}>
+              <img src={images.search} alt="search-box" />
+            </div>
+            <div className={cx('search-input')}>
+              <input placeholder="Tìm kiếm" name="search" />
+            </div>
+            <div className={cx('search-delete')}>
+              <img src={images.delete} alt="delete" />
+            </div>
+          </div>
+          <button className={cx('search-btn')}>
+            <img src={images.search} alt="btn" />
+          </button>
+          <div className={cx('voice-btn')}>
+            <img src={images.voice} alt="voice" title="Search with your voice" />
+          </div>
+        </div>
+        <div className={cx('action')}>
+          <div className={cx('setting-icon')}>
+            <img src={images.setting} alt="setting" />
+          </div>
+          <div className={cx('login-btn')}>
+            <div className={cx('user-icon')}>
+              <img src={images.user} alt="user" />
+            </div>
+            <div className={cx('login-content')}>SIGN IN</div>
+          </div>
+        </div>
       </header>
     </div>
   );
